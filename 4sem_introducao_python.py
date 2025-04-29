7ed05c85d287333eaa9deabda5522da097f32e8e
Rafael Honorato <114627686+rafaelhonorato03@users.noreply.github.com>
2025-04-28 22:39:03 -0300

    Update 4sem_introducao_python.py

--- 4sem_introducao_python.py
+++ 4sem_introducao_python.py
@@ -20,3 +20,15 @@ plt.show()
 arquivo = 'https://raw.githubusercontent.com/JeffSackmann/tennis_atp/master/atp_matches_2021.csv'
 dados = pd.read_csv(arquivo, sep=',')
 print(dados.head())
+print(dados.columns)
+print(dados.info())
+
+# Plotando os dados
+#eda = sv.analyze(dados)
+#eda.show_html('eda.html') - Esta dando erro, pois a versão não é compatível com o Nunpy
+
+# Analise de correlação de Pearson
+mask = np.triu(np.ones_like(dados.corr(), dtype=bool))
+plt.figure(figsize=(20, 20))
+sns.heatmap(dados.corr(), mask=mask, cmap='coolwarm', annot=True, fmt='.2f')
+plt.show()
