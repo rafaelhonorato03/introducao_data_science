a6be38aa2bcd5ddcbdd222aede228221882e1af2
Rafael Honorato <114627686+rafaelhonorato03@users.noreply.github.com>
2025-05-02 10:38:17 -0300

    Create 4_sem_exerc.py

---
+++ 4_sem_exerc.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import sweetviz as sv
+
+df = pd.read_csv('mcu dataset.csv')
+print(df.head())
+
+correlacao = df.select_dtypes(include='number').corr()
+print(correlacao)
+
+sns.heatmap(correlacao, annot=True, cmap='coolwarm')
+plt.title('Correlação entre variáveis numéricas')
+plt.tight_layout()
+plt.show()
