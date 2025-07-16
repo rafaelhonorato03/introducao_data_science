728c04fb224bb50a730bbfb78dbe5738a65da594
Rafael Honorato <114627686+rafaelhonorato03@users.noreply.github.com>
2025-07-16 20:18:27 -0300

    Create python_basics.py

---
+++ src/python_basics/python_basics.py
@@ -0,0 +1,37 @@
+from datetime import date
+
+idade = int(input("Digite sua idade: "))
+
+if idade > 18:
+    print("Você é maior de idade")
+elif idade == 18:
+    print("Você não é nem maior, nem menor de idade!")
+else:
+    print("Você é menor de idade")
+
+# Outra forma de apresentar condicionais, mais simples e direta
+print("Maior de idade") if idade >= 18 else print("Menor de didade")
+
+#switch case
+a = "Rafael"
+match a:
+    case "João":
+        print("Não é Rafael")
+    case "Rafael":
+        print("É Rafael")
+
+# Provinha de teste
+ano_nascimento = int(input("Qual o seu ano de nascimento? "))
+data_atual = date.today()
+data_atual = data_atual.year
+idade = data_atual - ano_nascimento
+
+if idade >= 18:
+    print(f"Você é maior e tem {idade} anos de idade")
+else:
+    print(f"Você é menor, pois tem {idade} anos")
+
+if idade >=18:
+    print("Digite o seu título de eleitor: ")
+else:
+    print("Digite o documento do seu resposável")
